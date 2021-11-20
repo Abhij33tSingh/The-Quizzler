94401d04a2fa427ff10e5792656ddde311909870
Dhiman Singha <imt_2019033@iiitm.ac.in>
2021-11-20 09:59:53 +0530

    Beautified the nav bar and started working on homepage

--- ClientSide/src/components/SelectQuiz.jsx
+++ ClientSide/src/components/SelectQuiz.jsx
@@ -1,38 +1,27 @@
 import { useNavigate } from "react-router-dom";
 //import AttemptQuiz from "./AttemptQuiz";
-const SelectQuiz = (props) =>{
-    const navigate = useNavigate();
-    const attempt = () =>{
-        return(
-            <div>
-                {navigate("/attempt/" + props.id +"/"+ props.q._id)
-                //console.log(props.q.allQuestions)
-                }
-            </div>
-        )
-    }
+const SelectQuiz = (props) => {
+  const navigate = useNavigate();
+  const attempt = () => {
     return (
-        <div>
-            <tbody>
-                <td className = "test">
-                    {
-                        props.q.title
-                    }
-                </td>
-                <td className = "test">
-                    {
-                        props.q.description
-                    }
-                </td>
-                <td className = "test">
-                    <button onClick = {attempt}>
-                        Attempt Quiz
-                    </button>
-                </td>
-            </tbody>
-        </div>
-    )
+      <div>
+        {
+          navigate("/attempt/" + props.id + "/" + props.q._id)
+          //console.log(props.q.allQuestions)
+        }
+      </div>
+    );
+  };
+  return (
+    <div className="QuizContainer">
+      <p className="test">{props.q.title}</p>
+      <p className="test">{props.q.description}</p>
 
-}
+      <button className="Attempt Button" onClick={attempt}>
+        Attempt Quiz
+      </button>
+    </div>
+  );
+};
 
 export default SelectQuiz;
