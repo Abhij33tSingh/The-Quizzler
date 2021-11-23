fa4fad0d8574e4608bf39f00bb7ba99f6d44ae30
Dhiman Singha <imt_2019033@iiitm.ac.in>
2021-11-23 17:20:27 +0530

    Started Designing the Attempter Result page

--- ClientSide/src/components/AttempterResults.jsx
+++ ClientSide/src/components/AttempterResults.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Axios from "axios";
 import GetUserDetails from "./GetUserDetails";
+import userIcons from "../Images/gamer.png";
 
 const AttempterResults = () => {
+  const navigate = useNavigate();
   const [attempterIDs, setAttempterIDs] = useState([]);
+  const [userData, setUserData] = useState([]);
   const params = useParams();
   useEffect(() => {
     Axios.get("http://localhost:3001/getQuiz/" + params.quizId).then(
@@ -12,7 +15,12 @@ const AttempterResults = () => {
         setAttempterIDs(response.data.attempterIDs);
       }
     );
-  }, []);
+    Axios.get("http://localhost:3001/getUser/" + params.hostId).then(
+      (response) => {
+        setUserData(response.data);
+      }
+    );
+  }, [params.hostId, params.quizId]);
 
   let viewing = () => {
     if (params.viewerId === params.hostId) {
@@ -34,11 +42,41 @@ const AttempterResults = () => {
 
   return (
     <div>
-      {viewing()}
-      <div>
-        {attempterIDs.map((attempterID) => {
-          return <GetUserDetails userId={attempterID} quizId={params.quizId} />;
-        })}
+      {/* {viewing()} */}
+      <header>
+        <h1 onClick={() => navigate("/homepage/" + params.viewerId)}>
+          Quizzler
+        </h1>
+
+        <div className="ProfilenavbarItems">
+          <nav onClick={() => navigate("/createquiz/" + params.viewerId)}>
+            Create a Quiz
+          </nav>
+          <nav onClick={() => navigate("/AboutUs/" + params.viewerId)}>
+            About us
+          </nav>
+          <nav onClick={() => navigate("/homepage/" + params.viewerId)}>
+            Start Quiz
+          </nav>
+          <nav onClick={() => navigate("/profile/" + params.viewerId)}>
+            {userData.name}
+          </nav>
+        </div>
+      </header>
+      <div className="profilePage">
+        <div className="IntroSection">
+          <img className="UserImage" src={userIcons} alt="user icon" />
+          <div>
+            <h1 className="Greetings">Hello {userData.name}</h1>
+          </div>
+        </div>
+        <div>
+          {attempterIDs.map((attempterID) => {
+            return (
+              <GetUserDetails userId={attempterID} quizId={params.quizId} />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
